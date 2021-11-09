f1c455ab5a4574bd5626d7a8a2c828bf2cd44d9f
Ian F <ian.fiddeman@gmail.com>
2021-11-09 22:15:25 +0000

    tidying

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,12 @@
 const request = require('request')
+const config = require('../config.json')
 
-const url = 'http://api.weatherstack.com/current?access_key=aa8608f14bfc9487eafcf22652135b23&&query=37.8267,-122.4233'
+
+//const url = 'http://api.weatherstack.com/current?access_key=aa8608f14bfc9487eafcf22652135b23&query=37.8267,-122.4233'
+const baseUrl = 'http://api.weatherstack.com/current?'
+const location = '37.8267,-122.4233'
+const url = baseUrl + 'access_key=' + config.api_key + '&&query=' + location;
+
 
 request({ url: url, json: true }, (error, response) => {
     //console.log(response.body.current);
@@ -8,4 +14,5 @@ request({ url: url, json: true }, (error, response) => {
     const precip = response.body.current.precip;
     const desc = response.body.current.weather_descriptions;
     console.log(`${desc[0]}. It's currently ${temp} degrees out. There is a ${precip}% chance of rain.`);
+
 })
