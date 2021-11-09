0ff5a3894053200185b17eefc5dc984667c8718c
Ian F <ian.fiddeman@gmail.com>
2021-11-09 22:28:33 +0000

    tidying

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,6 @@ const request = require('request')
 const config = require('../config.json')
 
 
-//const url = 'http://api.weatherstack.com/current?access_key=aa8608f14bfc9487eafcf22652135b23&query=37.8267,-122.4233'
 const baseUrl = 'http://api.weatherstack.com/current?'
 const location = '37.8267,-122.4233'
 const url = baseUrl + 'access_key=' + config.api_key + '&&query=' + location;
