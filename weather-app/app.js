1ae87f44c856d0686e657155f702ca23640a6cc1
Ian F <ian.fiddeman@gmail.com>
2021-11-11 09:01:57 +0000

    LA lat long added

--- weather-app/app.js
+++ weather-app/app.js
@@ -15,9 +15,15 @@ request({ url: url, json: true }, (error, response) => {
     console.log(`${desc[0]}. It's currently ${temp} degrees out. There is a ${precip}% chance of rain.`);
 
 })
-
+//MapBox API
 const mapBoxBaseURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'
-const mapBoxLocation = 'Los%20Angeles'
-const mapBoxURL = mapBoxBaseURL + mapBoxLocation + '.json?access_token=' + config.mapBoxKey;
+const mapBoxLocation = 'Los Angeles'
+const mapBoxURL = mapBoxBaseURL + mapBoxLocation + '.json?access_token=' + config.mapBoxKey
+
+request({ url: mapBoxURL, json:true}, (error, response) => {
+    const lat = response.body.features[0].center[0];
+    const lon = response.body.features[0].center[1];
+    console.log("For LA, the lat is: " + lat, " and the long is " + lon);
+})
 
 console.log(mapBoxURL)
